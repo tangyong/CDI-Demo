39398b8dfc90f48bb162f7f5c4b268f168d7cad2
tangyong <tangyong@cn.fujitsu.com>
2012-07-30 14:12:18 +0900

    [CDI-DEMO/SE]Sample4 are added

---
+++ se/sample4/src/main/java/org/cdi/advocacy/AutomatedTellerMachineImpl.java
@@ -0,0 +1,32 @@
+package org.cdi.advocacy;
+
+import java.math.BigDecimal;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named("atm")
+public class AutomatedTellerMachineImpl implements AutomatedTellerMachine {
+	
+	private ATMTransport transport;
+
+	@Inject
+    public AutomatedTellerMachineImpl(ATMTransport transport){
+    	this.transport = transport;
+    }
+
+	public void deposit(BigDecimal bd) {
+		System.out.println("deposit called");
+		
+		transport.communicateWithBank(null);
+		
+	}
+
+	public void withdraw(BigDecimal bd) {
+		System.out.println("withdraw called");
+
+		transport.communicateWithBank(null);
+
+	}
+
+}
---
+++ se/sample4/src/main/java/org/cdi/advocacy/SuperFastAtmTransport.java
@@ -0,0 +1,11 @@
+package org.cdi.advocacy;
+
+import javax.enterprise.inject.Alternative;
+
+@Alternative
+public class SuperFastAtmTransport implements ATMTransport {
+
+	public void communicateWithBank(byte[] datapacket) {
+		System.out.println("communicating with bank via the Super Fast transport " );
+	}
+}
